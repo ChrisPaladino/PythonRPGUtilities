8f9916c1407e707eec45ff8d6e58095f60d47d72
CAP882 <160057433+ChrisPaladino@users.noreply.github.com>
2025-05-12 18:08:12 -0400

    Add missing CharactersTab component

---
+++ src/components/CharactersTab.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+function CharactersTab() {
+  return (
+    <Box>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Characters & Threads
+      </Typography>
+      {/* Content will be added here later */}
+    </Box>
+  )
+}
+
+export default CharactersTab
