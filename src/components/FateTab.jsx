fff581b309ad45bda8f685e8b9b170ed988e53ac
CAP882 <160057433+ChrisPaladino@users.noreply.github.com>
2025-05-12 18:07:47 -0400

    Set up React RPG web application

---
+++ src/components/FateTab.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+function FateTab() {
+  return (
+    <Box>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Fate & Oracles
+      </Typography>
+    </Box>
+  )
+}
+
+export default FateTab
